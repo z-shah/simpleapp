2caf54c4d3e97dba9b3207e995721eaf8f414dd5
zainabs <zainab.sanaa@contino.io>
2019-04-02 22:23:02 +1100

    add appinfo version

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.set('json spaces', 2);
-app.get('/version', function(req, res) {
-    res.json({
-        "myapplication": 
-        [{
-            "version": "1.0",
-            "description": "pre-interview technical test",
-            "lastcommitsha": "abc57858585"
-        }]});
+'use strict';
+
+let jsonData = require('./build.json');
+
+var lastcommitsha = jsonData.id; 
+var version = jsonData.version;
+
+app.set('json spaces', 2)
+app.get('/appInfo', function(req, res) {
+
+    var data = ({
+        myapplication: {
+            version: jsonData.id,
+            description: "This is a sample app",
+            lastcommitsha: jsonData.version,
+        }
+        
+      });
+    res.status(200).send(data);
+
 });
+
 app.listen(port, () => console.log(`Sample app running on port ${port}!`))
