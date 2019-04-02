2038c2bfa2d5dd6bbf8be0967506b2512c975ce9
zainabs <zainab.sanaa@contino.io>
2019-04-03 02:22:22 +1100

    update newman tests

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 'use strict';
 
 let jsonData = require('./build.json');
 
 app.set('json spaces', 2)
-app.get('/appInfo', function(req, res) {
+app.get('/healthcheck', function(req, res, next) {
 
-    var data = ({
-        myapplication: {
+    var data = {
+        myapplication: [
+            {
             version: jsonData.version,
             description: "pre-interview technical test",
             lastcommitsha: jsonData.id,
-        }
-        
-      });
+            }
+        ]
+      };
+    res.set('Content-Type', 'application/json');
     res.status(200).send(data);
-
 });
 
 app.listen(port, () => console.log(`Sample app running on port ${port}!`))
